330d7cb4f4aaa9731ebbe0e972e0837555a97639
eslamghanm <eg637899@gmail.com>
2025-11-09 16:32:13 +0200

    fix-auth-conflicts

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
-            'role' => RoleMiddleware::class
-
-
+            'role' => RoleMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
